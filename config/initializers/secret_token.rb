06f6e86598c17720736913f36ed86ed269e181d0
Travis Jeffery <travisj@nulogy.com>
2012-01-09 03:04:00 +0000

    Engage

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notebox::Application.config.secret_token = '86a3f80f87cbdeefd3aea850901f432b9a79cdfe35c6d873fe37ac02be2a336858927a8d65b08bff4101e1bb98df3d04364358d05d517cc20c7d0a3b71c77aab'
