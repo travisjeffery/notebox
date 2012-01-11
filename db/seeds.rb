a207f24163d7292d07df9d83a21a18ee371bbb45
Travis Jeffery <travisjeffery@gmail.com>
2012-01-10 21:53:47 -0500

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+notes = Note.create([
+  { name: "Core Animation Demo WWDC 2007 - YouTube", description: "Always worth a re-watch: Steve Jobs demonstrating Core Animation at WWDC 2007." },
+  { name: "Steve Jobs: A Few Memories | Xconomy", description: %(When I first met Steve Jobs nearly 25 years ago I was struck by him explaining to me that NeXT was what he "wanted to do with his thirties".)},
+  { name: "NeXTSTEP Release 3 Demo - YouTube", description: "Steve Jobs demoing NeXTSTEP R3, including Interface Builder and Database Kit!" }
+])
